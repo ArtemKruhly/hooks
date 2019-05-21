3e310763049b87de6324da5460b80894b6cbfbc5
kruglyyap <artem.kruglyy@oxagile.com>
2019-05-21 15:08:19 +0300

    init  commit

---
+++ src/Content.js
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+
+export default class Header extends React.Component {
+
+		render() {
+				return (
+						<div style={{ marginBottom: '100px' }}>
+								{!!localStorage.getItem('isAuth') || localStorage.getItem('isAuth') === 'true' ?
+								<ul>
+										<li>
+												<NavLink to={'/video'} activeClassName='nav_active'>Video Player</NavLink>
+										</li>
+										<li>
+												<NavLink to={'/audio'} activeClassName='nav_active'>Audio Player</NavLink>
+										</li>
+										<li>
+												<NavLink to={'/location'} activeClassName='nav_active'>Geolocation</NavLink>
+										</li>
+										<li>
+												<NavLink to={'/hook'} activeClassName='nav_active'>Hook</NavLink>
+										</li>
+								</ul>
+										:
+										null
+								}
+						</div>
+				);
+		}
+}
